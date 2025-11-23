fb3a1a466fa904d20b5bbfd092912ac788f70155
LocNT <locnt1501@gmail.com>
2025-11-23 12:05:15 +0700

    fix: add alear when random

--- basic/random-color/common.js
+++ basic/random-color/common.js
@@ -1,13 +1,21 @@
 const code = document.getElementById("code")
-const btnRandom = document.querySelector("[data-cmnjs-random")
+const btn = document.getElementById("copyBtn");
+const btnRandom = document.querySelector("[data-cmnjs-random]")
 let color = ""
 
 const randomColor = () => {
   const chars = '0123456789ABCDEF';
-  color = '#' +  Array.from({ length: 6 }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
+  color = '#' + Array.from({ length: 6 }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
   code.innerHTML = color
   document.body.style.backgroundColor = color
 }
+btn.addEventListener("click", () => {
+  const text = code.textContent;
 
+  navigator.clipboard.writeText(text).then(() => {
+    btn.textContent = "Copied!";
+    setTimeout(() => btn.textContent = "Copy", 1000);
+  });
+});
 btnRandom.addEventListener("click", randomColor)
 document.addEventListener("DOMContentLoaded", randomColor)
